5609cd9572ed665019d98a461d2f0fd751f92484
Tuan Bach Van <bachvtuan@gmail.com>
2026-09-18 17:31:50 +0700

    Support own ADMIN_RESUMABLE_STORAGE and ADMIN_RESUMABLE_CHUNK_STORAGE

--- admin_async_upload/storage.py
+++ admin_async_upload/storage.py
@@ -1,7 +1,7 @@
 import datetime
 import posixpath
+from django.utils.module_loading import import_string
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
 from django.utils.encoding import force_str
 
 
@@ -13,11 +13,11 @@ class ResumableStorage(object):
                                                 'django.core.files.storage.FileSystemStorage')
 
         # get_storage_class removed in Django 5.1:
-        # self.chunk_storage_class_name = getattr(
-        #     settings,
-        #     'ADMIN_RESUMABLE_CHUNK_STORAGE',
-        #     'django.core.files.storage.FileSystemStorage'
-        # )
+        self.chunk_storage_class_name = getattr(
+            settings,
+            'ADMIN_RESUMABLE_CHUNK_STORAGE',
+            'django.core.files.storage.FileSystemStorage'
+        )
 
     def get_chunk_storage(self, *args, **kwargs):
         """
@@ -26,10 +26,9 @@ class ResumableStorage(object):
         Chunk storage should be highly available for the server as saved chunks must be copied by the server
         for saving merged version in persistent storage.
         """
-        # get_storage_class removed in Django 5.1:
-        # storage_class = get_storage_class(self.chunk_storage_class_name)
-        # return storage_class(*args, **kwargs)
-        return FileSystemStorage(*args, **kwargs)
+        chunk_storage_cls = import_string(self.chunk_storage_class_name)
+        return chunk_storage_cls(*args, **kwargs)
+        
 
     def get_persistent_storage(self, *args, **kwargs):
         """
@@ -38,10 +37,8 @@ class ResumableStorage(object):
 
         Defaults to django.core.files.storage.FileSystemStorage.
         """
-        # get_storage_class removed in Django 5.1:
-        # storage_class = get_storage_class(self.persistent_storage_class_name)
-        # return storage_class(*args, **kwargs)
-        return FileSystemStorage(*args, **kwargs)
+        persistent_storage_cls = import_string(self.persistent_storage_class_name)
+        return persistent_storage_cls(*args, **kwargs)
 
     def full_filename(self, filename, upload_to, instance=None):
         if callable(upload_to):
